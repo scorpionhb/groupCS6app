2f42ff20ede6c509daf1ddd691d13090cff11eac
petyonachev <petyonachev@abv.bg>
2016-12-04 23:49:06 +0000

    Adding log in fields

--- mapPage.php
+++ mapPage.php
@@ -9,6 +9,7 @@
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
         <link rel="stylesheet" type="text/css" href="homeStyle.css">
         <link rel="stylesheet" type="text/css" href="mapStyle.css">
+        <script src="homeJS.js"></script>
         <script
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMI5iwxHNqNnZSvbVJkE656xZoKPpfBfc "
                 type="text/JavaScript">
@@ -66,8 +67,19 @@
                         <li><a href="#">Map</a></li>
                     </ul>
 
-                    <ul class="nav navbar-nav navbar-right">
-                        <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <ul id="loginFields" class="nav navbar-nav navbar-right">
+                        <li><form id="signin" class="navbar-form navbar-right" role="form">
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                    <input id="username" type="username" class="form-control" name="username" value="" placeholder="Username">
+                                </div>
+
+                                <div class="input-group">
+                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                    <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+                                </div></form>
+                        </li>
+                        <li  onclick="logIn()"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
                         <li><a id="regButton" href="#">Register</a></li>
                     </ul>
                 </div>
