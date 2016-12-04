af17231201b4d78e22f677538b51a37c6b578e51
petyonachev <petyonachev@abv.bg>
2016-12-04 23:27:54 +0000

    Limiting Map drag test2

--- mapPage.php
+++ mapPage.php
@@ -22,24 +22,26 @@
                 maxZoom: 20,
                 mapTypeId: 'roadmap'
                 });
+
+                var allowedBounds = new google.maps.LatLngBounds(
+                    new google.maps.LatLng(-84.780861, 179.284397),
+                    new google.maps.LatLng(77.566551, 176.322804)
+                );
+                var lastValidCenter = map.getCenter();
+
+                google.maps.event.addListener(map, 'center_changed', function() {
+                    if (allowedBounds.contains(map.getCenter())) {
+                        // still within valid bounds, so save the last valid position
+                        lastValidCenter = map.getCenter();
+                        return;
+                    }
+
+                    // not valid anymore => return to last valid position
+                    map.panTo(lastValidCenter);
+                });
             }
 
-            var allowedBounds = new google.maps.LatLngBounds(
-                new google.maps.LatLng(-84.780861, 179.284397),
-                new google.maps.LatLng(77.566551, 176.322804)
-            );
-            var lastValidCenter = map.getCenter();
-
-            google.maps.event.addListener(map, 'center_changed', function() {
-                if (allowedBounds.contains(map.getCenter())) {
-                    // still within valid bounds, so save the last valid position
-                    lastValidCenter = map.getCenter();
-                    return;
-                }
-
-                // not valid anymore => return to last valid position
-                map.panTo(lastValidCenter);
-            });
+            
         </script>
     </head>
 <body onload ="load()">
