579ea012d266a79ca323d73065345c6450000c39
petyonachev <petyonachev@abv.bg>
2016-12-04 23:53:01 +0000

    Creating google map from a JS file instead from the mapPage.php

---
+++ mapJS.js
@@ -0,0 +1,29 @@
+/**
+ * Created by Sanctuum on 04-Dec-16.
+ */
+function load(){
+    var map = new google.maps.Map(document.getElementById("map"),{
+        center: new google.maps.LatLng(47.6145, -122.3418),
+        zoom: 12,
+        minZoom: 5,
+        maxZoom: 20,
+        mapTypeId: 'roadmap'
+    });
+
+    var allowedBounds = new google.maps.LatLngBounds(
+        new google.maps.LatLng(-81.780861, 179.284397),
+        new google.maps.LatLng(77.566551, 176.322804)
+    );
+    var lastValidCenter = map.getCenter();
+
+    google.maps.event.addListener(map, 'center_changed', function() {
+        if (allowedBounds.contains(map.getCenter())) {
+            // still within valid bounds, so save the last valid position
+            lastValidCenter = map.getCenter();
+            return;
+        }
+
+        // not valid anymore => return to last valid position
+        map.panTo(lastValidCenter);
+    });
+}
--- mapPage.php
+++ mapPage.php
@@ -10,39 +10,10 @@
         <link rel="stylesheet" type="text/css" href="homeStyle.css">
         <link rel="stylesheet" type="text/css" href="mapStyle.css">
         <script src="homeJS.js"></script>
+        <script src="mapJS.js"</script>
         <script
             src="https://maps.googleapis.com/maps/api/js?key=AIzaSyDMI5iwxHNqNnZSvbVJkE656xZoKPpfBfc "
                 type="text/JavaScript">
-        </script>
-        <script type ="text/JavaScript">
-            function load(){
-                var map = new google.maps.Map(document.getElementById("map"),{
-                center: new google.maps.LatLng(47.6145, -122.3418),
-                zoom: 12,
-                minZoom: 5,
-                maxZoom: 20,
-                mapTypeId: 'roadmap'
-                });
-
-                var allowedBounds = new google.maps.LatLngBounds(
-                    new google.maps.LatLng(-81.780861, 179.284397),
-                    new google.maps.LatLng(77.566551, 176.322804)
-                );
-                var lastValidCenter = map.getCenter();
-
-                google.maps.event.addListener(map, 'center_changed', function() {
-                    if (allowedBounds.contains(map.getCenter())) {
-                        // still within valid bounds, so save the last valid position
-                        lastValidCenter = map.getCenter();
-                        return;
-                    }
-
-                    // not valid anymore => return to last valid position
-                    map.panTo(lastValidCenter);
-                });
-            }
-
-            
         </script>
     </head>
 <body onload ="load()">
