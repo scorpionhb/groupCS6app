bef7e64e7a4c951e1dc948939b016a09d25ddba8
ACER <dasda>
2016-12-04 15:07:05 +0000

    adminer and dbconnect

---
+++ dbConnect.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ACER
+ * Date: 10/10/2016
+ * Time: 22:12
+ */
+?>
+
+<?php
+    define('DB_SERVER', 'us-cdbr-azure-southcentral-f.cloudapp.net');
+    define('DB_USERNAME', 'b20897870d42e6');
+    define('DB_PASSWORD', 'f2fdd194');
+    define('DB_DATABASE', 'cs6app_db');
+
+    $db = mysqli_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD, DB_DATABASE);
+?>
