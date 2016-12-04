5770014b034183a1f0fcd292115688d5698710b2
petyonachev <petyonachev@abv.bg>
2016-12-04 17:15:40 +0000

    removed duplicates

--- mapPage.php
+++ mapPage.php
@@ -16,7 +16,7 @@
             function load(){
                 var map = new google.maps.Map(document.getElementById("map"),{
                 center: new google.maps.LatLng(47.6145, -122.3418),
-                zoom: 13,
+                zoom: 12,
                 minZoom: 5,
                 maxZoom: 20,
                 mapTypeId: 'roadmap'
