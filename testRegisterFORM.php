59a41e51889b5044290a95bc33d0fbffc4e3cadb
monco21 <simeonov21@gmail.com>
2016-12-07 21:36:23 +0000

    adding club form

--- testRegisterFORM.php
+++ testRegisterFORM.php
@@ -27,7 +27,7 @@ if(isset($_POST['submit'])){
         $password = md5($password);
         //Insert the record
         $insert = $mysqli->query("INSERT INTO users(username,password,email,tel_number,type_of_user) VALUES ('$username','$password','$email','$telNumber','$typeOfUser')",
-            "INSERT INTO type_of_users(username, access_level,description) VALUES ('$username','0','NULL')");
+            "INSERT INTO type_of_users(username, access_level,description) VALUES ('$username','0',)");
         if($insert != true){
             $output = "There was a problem <br/>";
             $output .= $mysqli->error;
