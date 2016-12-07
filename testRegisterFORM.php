bdea5647c8108f5a382cc946d5257b0cd340c239
ACER <dasda>
2016-12-07 17:47:32 +0000

    register test

--- testRegisterFORM.php
+++ testRegisterFORM.php
@@ -39,6 +39,80 @@ if(isset($_POST['submit'])){
 }
 ?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Club Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" type="text/css" href="clubPageStyle.css">
+    <script src="homeJS.js"></script>
+</head>
+<body>
+<div id="wrapper">
+    <nav class="navbar navbar-fixed-top navbar-absolute navbar-transparent big">
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+                <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+                <img class="navbar-brand" src="https://github.com/scorpionhb/groupCS6app/blob/testBranch/LOGO1.png?raw=true"/>
+            </div>
+
+            <div class="collapse navbar-collapse" id="myNavbar">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="http://cs6testapp.azurewebsites.net/index.php">Clubs and Societies</a></li>
+                    <li><a href="http://cs6testapp.azurewebsites.net/healthNWell.php">Health and Well-being</a></li>
+                    <li><a href="http://cs6testapp.azurewebsites.net/mapPage.php">Map</a></li>
+                </ul>
+
+
+                <ul class="nav navbar-nav navbar-center">
+                    <li>
+                        <form id="search" class="navbar-form navbar-right" role="form">
+                            <div class="input-group">
+                                <input type="text" class="form-control" placeholder="Search">
+                                <div class="input-group-btn">
+                                    <button class="btn btn-default" type="submit">
+                                        <i class="glyphicon glyphicon-search"></i>
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </li>
+                </ul>
+
+
+                <ul id="loginFields" class="nav navbar-nav navbar-right">
+                    <li ><form id="signin" class="navbar-form navbar-right" role="form">
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-user"></i></span>
+                                <input id="username" type="text" class="form-control" name="username" value="" placeholder="Username">
+                            </div>
+
+                            <div class="input-group">
+                                <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
+                                <input id="password" type="password" class="form-control" name="password" value="" placeholder="Password">
+                            </div></form>
+                    </li>
+
+                    <li id="loginBut" onclick="logIn()"><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+                    <li><a id="regButton" href="#"><span class="glyphicon glyphicon-user"></span>Sign Up</a></li>
+                </ul>
+
+
+            </div>
+        </div>
+    </nav>
+
+
+
 <form method="post">
     <table>
         <tr>
@@ -58,8 +132,8 @@ if(isset($_POST['submit'])){
             <td><input type="email" name="email"/></td>
         </tr>
         <tr>
-            <td>Email:</td>
-            <td><input type="email" name="tel"/></td>
+            <td>Telephone:</td>
+            <td><input type="text" name="tel"/></td>
         </tr>
     </table>
 
