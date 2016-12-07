8d8556a4d7770e023d3e6263606a85b423f658ad
monco21 <simeonov21@gmail.com>
2016-12-07 17:00:08 +0000

    adding club form

--- testingSimpleForm.php
+++ testingSimpleForm.php
@@ -38,29 +38,29 @@ if(isset($_POST['submit'])){
 }
 ?>
 
-    <form method="post">
-        <table>
-            <tr>
-                <td>Username:</td>
-                <td><input type="text" name="username"/></td>
-            </tr>
-            <tr>
-                <td>Password:</td>
-                <td><input type="password" name="password"/></td>
-            </tr>
-            <tr>
-                <td>Repeat Password:</td>
-                <td><input type="password" name="rpassword"/></td>
-            </tr>
-            <tr>
-                <td>Email:</td>
-                <td><input type="email" name="email"/></td>
-            </tr>
-        </table>
+<form method="post">
+    <table>
+        <tr>
+            <td>Username:</td>
+            <td><input type="text" name="username"/></td>
+        </tr>
+        <tr>
+            <td>Password:</td>
+            <td><input type="password" name="password"/></td>
+        </tr>
+        <tr>
+            <td>Repeat Password:</td>
+            <td><input type="password" name="rpassword"/></td>
+        </tr>
+        <tr>
+            <td>Email:</td>
+            <td><input type="email" name="email"/></td>
+        </tr>
+    </table>
 
-        <input type="submit" name="submit" value="Register"/>
-    </form>
+    <input type="submit" name="submit" value="Register"/>
+</form>
 
 <?PHP
 echo $output;
-?>
+?>/
