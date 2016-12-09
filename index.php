20d9add786af72b65a5123ce89f610d4750d61c6
monco21 <simeonov21@gmail.com>
2016-12-09 22:55:58 +0000

    adding club form

--- index.php
+++ index.php
@@ -90,7 +90,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET'){
 
                     </li>
 
-                    <li "><a  id="regButton" href="#"><span
+                    <li "><a  id="regButton" href="testRegisterFORM.php"><span
                                 class="glyphicon glyphicon-user"></span>Sign Up</a></li>
                 </ul>
             </div>
