a7ab0ff2505aefef40efdacf3d26bea38a2a8eb8
monco21 <simeonov21@gmail.com>
2016-12-11 14:39:16 +0000

    adding club form

--- adminPanel.php
+++ adminPanel.php
@@ -46,11 +46,11 @@ $mysqli = NEW MySQLi('us-cdbr-azure-southcentral-f.cloudapp.net', 'b20897870d42e
     while ($row = $result->fetch_array()) {
         echo"<form action='adminPanel.php' method='post'>";
         echo"<tr>";
-        echo"<td>" . "<input type='text' name='UserName' value=" . $row[username] . " </td>";
-        echo"<td>" . "<input type='text' name='UserType' value=" . $row[type_of_user] . " </td>";
-        echo"<td>" . "<input type='text' name='UserPhone' value=" . $row[tel_number] . " </td>";
-        echo"<td>" . "<input type='text' name='UserEmail' value=" . $row[email] . " </td>";
-        echo"<td>" . "<input type='hidden' name='hidden' value=" . $row[username] . " </td>";
+        echo"<td>" . "<input type='text' name='UserName' value=" . $row['username'] . " </td>";
+        echo"<td>" . "<input type='text' name='UserType' value=" . $row['type_of_user'] . " </td>";
+        echo"<td>" . "<input type='text' name='UserPhone' value=" . $row['tel_number'] . " </td>";
+        echo"<td>" . "<input type='text' name='UserEmail' value=" . $row['email'] . " </td>";
+        echo"<td>" . "<input type='hidden' name='hidden' value=" . $row['username'] . " </td>";
         echo"<td>" . "<input type='submit' name='update' value='update'>" . " </td>";
         echo"</tr>";
         echo"</form>";
