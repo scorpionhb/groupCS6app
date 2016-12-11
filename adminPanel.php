8d73e29b49cb534a0e3d45ac1a8563078e6da559
monco21 <simeonov21@gmail.com>
2016-12-11 18:03:25 +0000

    adding club form

--- adminPanel.php
+++ adminPanel.php
@@ -30,34 +30,35 @@ $mysqli = NEW MySQLi('us-cdbr-azure-southcentral-f.cloudapp.net', 'b20897870d42e
             <th>User Email</th>
         </tr>
     <?php
+    if(isset($_POST['submit'])) {
+        $username = $mysqli->real_escape_string($_POST['UserName']);
+        $email = $mysqli->real_escape_string($_POST['UserEmail']);
+        $telNumber = $mysqli->real_escape_string($_POST['UserPhone']);
+        $typeOfUser = $mysqli->real_escape_string($_POST['UserType']);
+        $hiddenValue = $mysqli->real_escape_string($_POST['hidden']);
 
-    if(isset($_POST['submit'])){
 
-        $UpdateQuery = $mysqli->query("UPDATE users SET username='$_POST[UserName]', email='$_POST[UserEmail]' WHERE username='$_POST[hidden]'");
-        if($UpdateQuery != true){
-            $output = "There was a problem <br/>";
-            $output .= $mysqli->error;
-        }else{
-            $output = "You have been registered!";
+        $UpdateQuery = $mysqli->query("UPDATE users SET email='$email',tel_number='$telNumber',type_of_user='$typeOfUser' WHERE userID='$hiddenValue'");
+
+
+        $sql_query = "SELECT * FROM users";
+        $result = $mysqli->query($sql_query);
+        while ($row = $result->fetch_array()) {
+            echo "<form action='adminPanel.php' method='post'>";
+            echo "<tr>";
+            echo "<td>" . "<input type='text' name='UserName' value=" . $row['username'] . " </td>";
+            echo "<td>" . "<input type='text' name='UserType' value=" . $row['type_of_user'] . " </td>";
+            echo "<td>" . "<input type='text' name='UserPhone' value=" . $row['tel_number'] . " </td>";
+            echo "<td>" . "<input type='text' name='UserEmail' value=" . $row['email'] . " </td>";
+            echo "<td>" . "<input type='hidden' name='hidden' value=" . $row['userID'] . " </td>";
+            echo "<td>" . "<input type='submit' name='update' value='update'>" . " </td>";
+            echo "</tr>";
+            echo "</form>";
+
         }
-    };
-
-    $sql_query = "SELECT * FROM users";
-    $result = $mysqli->query($sql_query);
-    while ($row = $result->fetch_array()) {
-        echo"<form action='adminPanel.php' method='post'>";
-        echo"<tr>";
-        echo"<td>" . "<input type='text' name='UserName' value=" . $row['username'] . " </td>";
-        echo"<td>" . "<input type='text' name='UserType' value=" . $row['type_of_user'] . " </td>";
-        echo"<td>" . "<input type='text' name='UserPhone' value=" . $row['tel_number'] . " </td>";
-        echo"<td>" . "<input type='text' name='UserEmail' value=" . $row['email'] . " </td>";
-        echo"<td>" . "<input type='hidden' name='hidden' value=" . $row['username'] . " </td>";
-        echo"<td>" . "<input type='submit' name='update' value='update'>" . " </td>";
-        echo"</tr>";
-        echo"</form>";
+        echo $output;
 
     }
-    echo $output;
     ?>
 
     </table>
