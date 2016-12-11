f1c72594e3fd988de8e7a60bdd82827223b02c9d
monco21 <simeonov21@gmail.com>
2016-12-11 20:21:20 +0000

    adding club form

--- adminPanel.php
+++ adminPanel.php
@@ -48,9 +48,10 @@ echo $output= null;
             echo "<td>" . "<input type='text' name='UserType' value=" . $row['type_of_user'] . " </td>";
             echo "<td>" . "<input type='text' name='UserPhone' value=" . $row['tel_number'] . " </td>";
             echo "<td>" . "<input type='text' name='UserEmail' value=" . $row['email'] . " </td>";
+            echo "<td>" . "<input type='text' name='UserAccess' value=" . $row['access_level'] . " </td>";
             echo "<input type='hidden' name='hidden' value=" . $row['userID'] . " ";
             echo "<td>" . "<input type='submit' name='update' value='update'>" . " </td>";
-            echo "<td>" . "<input type='text' name='UserAccess' value=" . $row['access_level'] . " </td>";
+
             echo "</tr>";
             echo "</form>";
 
