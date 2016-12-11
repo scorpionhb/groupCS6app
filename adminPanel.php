a4457ce0067f810a178623bba53a3ab5b55b54a4
monco21 <simeonov21@gmail.com>
2016-12-11 20:16:54 +0000

    adding club form

--- adminPanel.php
+++ adminPanel.php
@@ -37,8 +37,10 @@ echo $output= null;
         $UpdateQuery = $mysqli->query("UPDATE users SET email='$_POST[UserEmail]',tel_number='$_POST[UserPhone]',type_of_user='$_POST[UserType]' WHERE userID='$_POST[hidden]'");
 
 
-        $sql_query = "SELECT * FROM users";
+        $sql_query = "SELECT * FROM users, type_of_user";
         $result = $mysqli->query($sql_query);
+        $out = $mysqli->query($sql_query1);
+    while($row=$out->fetch_array()){
         while ($row = $result->fetch_array()) {
             echo "<form action='adminPanel.php' method='post'>";
             echo "<tr>";
@@ -46,12 +48,15 @@ echo $output= null;
             echo "<td>" . "<input type='text' name='UserType' value=" . $row['type_of_user'] . " </td>";
             echo "<td>" . "<input type='text' name='UserPhone' value=" . $row['tel_number'] . " </td>";
             echo "<td>" . "<input type='text' name='UserEmail' value=" . $row['email'] . " </td>";
+            echo "<td>" . "<input type='text' name='UserAccess' value=" . $row['access_level'] . " </td>";
             echo "<input type='hidden' name='hidden' value=" . $row['userID'] . " ";
             echo "<td>" . "<input type='submit' name='update' value='update'>" . " </td>";
-
+            echo "</tr>";
+            echo "</form>";
 
         }
-
+        }
+/*
         $sql_query1 = "SELECT * FROM type_of_user";
         $out = $mysqli->query($sql_query1);
         while($row=$out->fetch_array()){
@@ -60,7 +65,7 @@ echo $output= null;
             echo "</form>";
         }
 
-
+*/
 
     ?>
 
