73054ef5b45da8cba677db278999fec412e04ad0
monco21 <simeonov21@gmail.com>
2016-12-11 11:44:03 +0000

    adding club form

---
+++ AmindPanel.php
@@ -0,0 +1,28 @@
+<?php
+$mysqli = NEW MySQLi('us-cdbr-azure-southcentral-f.cloudapp.net', 'b20897870d42e6', 'f2fdd194', 'cs6app_db');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>My form</title>
+</head>
+<body>
+<h1>User List</h1>
+<p>The following users have registered on the site:</p>
+<ul>
+    <?
+    $sql_query="SELECT*FROM users";
+    $result = $db->query($sql_query);
+    while($row = $result->fetch_array()){
+        $username = $row['username'];
+
+        echo "<li>{$username}</li>";
+    }
+    ?>
+</ul>
+
+</body>
+</html>
