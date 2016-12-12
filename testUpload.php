432e67f93f63b7f9b4fab82a96009ad215c6f5c1
azifchyy <azifchyy>
2016-12-12 20:02:22 +0200

    testupload

---
+++ testUpload.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: azifchyy
+ * Date: 12.12.2016 г.
+ * Time: 19:56
+ */
+
+include(dbConnect.php);
+$title = $_POST["title"];
+$info = $_POST["info"];
+
+$sql = "INSERT INTO news(content,title) VALUES ('$info', '$title')";
+
+$result = $db->query($sql);
