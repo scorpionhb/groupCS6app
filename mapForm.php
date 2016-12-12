47fd4ea36349174e1ed33c2cb68e79e4519605b6
petyonachev <petyonachev@abv.bg>
2016-12-12 18:22:23 +0000

    Adding map filter and custom icons

--- mapForm.php
+++ mapForm.php
@@ -10,21 +10,23 @@ $image = $_POST["image"];
 $lat = $_POST["latitude"];
 $lon = $_POST["longitude"];
 $imageCheck = substr($image, -3);
+$warning = "";
 
 if(empty($type) OR empty($name) OR empty($address) OR empty($desc) OR empty($lat) OR empty($lon)){
-    echo("Please fill in all the required fields!");
+    $warning = "Please fill in all the required fields!";
+
 }
 else if($type != "Viewpoint" OR $type != "Club" OR $type != "HistoricalLandmark" OR $type != "Route"){
-    echo("Please input any of the following types(Viewpoint,Club,HistoricalLandmark,Route");
+    $warning = "Please input any of the following types(Viewpoint,Club,HistoricalLandmark,Route";
 }
 else if(strlen($name > 50)){
-    echo("Name cannot be longer than 50 characters.");
+    $warning = "Name cannot be longer than 50 characters.";
 }
 else if(strlen($address) > 100){
-    echo("Address cannot be longer than 100 characters.");
+    $warning = "Address cannot be longer than 100 characters.";
 }
 else if($imageCheck != jpg OR $imageCheck != "png"){
-    echo("Invalid url format");
+    $warning = "Invalid url format";
 }
 else{
     $sql = "INSERT INTO locations(type,name,address,description,history,geological,imgURL,latitude,longitude) VALUES('$type','$name','$address','$desc','$history','$geological','$image','$lat','$lon')";
@@ -41,6 +43,7 @@ else{
 
 </head>
 <body>
+    <h2 id="warning">.</h2>
     <form action="mapForm.php" method="post">
         <div class="form-group">
             <label for="inputType">Location Type</label>
